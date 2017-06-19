6f1ee5eca25640b492381ac8b7a6449b1e93469b
Jeff <mcmahan.jeff@gmail.com>
2017-06-18 21:13:49 -0500

    app,components,config & utils dir's added / routes.js added with base routes

---
+++ simplist/app/config/routes.js
@@ -0,0 +1,25 @@
+import React from "react";
+import router from "react-router";
+
+var Route = router.Route;
+var Router = router.Router;
+var hashHistory = router.hashHistory;
+var indexRoute = router.IndexRoute;
+
+var Login = require('../components/Login');
+var Welcome = require('../components/Welcome');
+var NewList = require('../components/NewList');
+var SavedList = require('../components/SavedList');
+var SharedList = require('../components/SharedList');
+
+module.exports = (
+    <Router history={hashHistory}>
+        <Route path="/" component={Login}>
+            <Route path="welcome" component={Welcome} />
+            <Route path="newlist" component={NewList} />
+            <Route path="savedlist" component={SavedList} />
+            <Route path="sharedlist" component={SavedList} />
+        </Route>
+    </Router>
+
+);
