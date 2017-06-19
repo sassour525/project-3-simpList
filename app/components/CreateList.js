d9b26ad087fe4030cea6950370470f85503f9e90
Shawn Assour <sassour@Shawns-MacBook-Pro.local>
2017-06-19 18:48:22 -0500

    added new components for pages

--- app/components/CreateList.js
+++ app/components/CreateList.js
@@ -1,35 +1,59 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import ListItem from './ListItem.js';
+import InputBar from './InputBar.js'
 
-class CreateList extends Component {
-	constructor() {
-		super();
-	}
+class CreateList extends Component{
 
-	render() {
-		return (
+    constructor(props){
+        super(props);
+        this.state = {
+            todoList:[],
+            toggleClick:(taskIndex) => {
+                console.log(taskIndex);
+                const newToDoList = [...this.state.todoList];
+                newToDoList[taskIndex].completed = !newToDoList[taskIndex].completed;
+                this.setState({
+                    todoList: newToDoList
+                });
+            },
+            clearTasks:()=>{
+                const uncompletedTasks = this.state.todoList.filter(function(todo) {
+                    return todo.completed == false;
+                });
+                this.setState({
+                    todoList: uncompletedTasks
+                });
+                console.log("we need to clear tasks... :( ");
+            },
+            addNewTask:(taskText)=>{
+               console.log(`${taskText} needs to be added to our list still...`);
+               if (taskText == ''){ return; } 
+               let newItem = {task: taskText, completed: false};
+               let currentList = [...this.state.todoList];
+               currentList.push(newItem);
+               this.setState({
+                    todoList: currentList
+               })
+            }
+        };    
+    };
+    render(){
+        return(
 			<div className="panel panel-default">
 				<div className="panel-heading">
 					<h5>Create a new list</h5>
 				</div>
 				<div className="panel-body">
-					<div className="row">
-						<input id="new-list-item-txt-box" type="text" placeholder="Enter a new list item" />
-						<button type="submit" id="add-list-item-btn">+</button>
-					</div>
-					<div className="row">
-						<input id="share-list-txt-box" type="text" placeholder="Enter the user's ID to share" />
-						<br />
-						<button type="submit" id="share-new-list-btn">Share List</button>
-					</div>
-					<div className="row">
-						<input id="new-list-name-box" type="text" placeholder="Enter a list name" />
-						<br />
-						<button type="submit" id="save-new-list-btn">Save List</button>
-					</div>
-				</div>
-			</div>
-		);
-	}
-}
+				<InputBar addTask={this.state.addNewTask} clearTasks={this.state.clearTasks}/>
+		            <div>
+		                {this.state.todoList.map((item,index)=>{
+		                    return <ListItem task={item.task} completed={item.completed} handleClick={this.state.toggleClick} key={index} taskIndex={index}/>
+		                })}
+		            </div>
+		        </div>
+		    </div>
+        )
+    }
+};
 
 export default CreateList;
