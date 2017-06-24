32d46bb3b64a04de3875190cf62666b3c6ff580e
Shawn Assour <sassour@Shawns-MacBook-Pro.local>
2017-06-24 12:44:49 -0500

    changed panel name for specific use case

--- simplist/app/app.js
+++ simplist/app/app.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 
 // Including our Panel and Container components
 import Login from "./components/Login";
-import Panel from "./components/Panel";
+import SavedPanel from "./components/SavedPanel";
 import Container from "./components/Container";
 import Profile from "./components/Profile";
 import CreateList from "./components/CreateList";
@@ -12,7 +12,7 @@ import CreateList from "./components/CreateList";
 // Creating an App component which renders a Panel inside of a Container
 const App = () => (
   <Container>
-    <Panel />
+    <SavedPanel />
   </Container>
 );
 
