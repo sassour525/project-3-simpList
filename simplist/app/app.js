a33c74271cb8c657abe4b041c5ea541dbe13fc26
Shawn Assour <sassour@Shawns-MacBook-Pro.local>
2017-06-24 12:55:52 -0500

    added new components and hide the logout until authenticated

--- simplist/app/app.js
+++ simplist/app/app.js
@@ -5,6 +5,7 @@ import ReactDOM from "react-dom";
 // Including our Panel and Container components
 import Login from "./components/Login";
 import SavedPanel from "./components/SavedPanel";
+import SharedPanel from "./components/SharedPanel";
 import Container from "./components/Container";
 import Profile from "./components/Profile";
 import CreateList from "./components/CreateList";
@@ -12,7 +13,7 @@ import CreateList from "./components/CreateList";
 // Creating an App component which renders a Panel inside of a Container
 const App = () => (
   <Container>
-    <SavedPanel />
+    <Login />
   </Container>
 );
 
