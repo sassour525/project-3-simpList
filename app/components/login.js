9c04730ddb4a11fdc31b207e77006ad2c89e9751
Jeff <mcmahan.jeff@gmail.com>
2017-06-25 16:36:42 -0500

    more style stuff

--- app/components/login.js
+++ app/components/login.js
@@ -22,10 +22,18 @@ class Login extends Component {
 		const buttonStyle={
 			margin:12,
 		};
-		const paperStyle={
+		const paperStyle1={
 			height: 300,
-			width: 500,
+			width: 550,
 			margin: 20,
+			textAligh: 'center',
+			display:'inline-block',
+		};
+		const paperStyle2={
+			padding: 5,
+			height: 100,
+			width: 150,
+			margin: 30,
 			textAligh: 'center',
 			display:'inline-block',
 		};
@@ -33,7 +41,7 @@ class Login extends Component {
 			margin:12,
 		};
 		// end styling
-		
+
 		return (
 			<div className="panel panel-default">
 			
@@ -41,18 +49,27 @@ class Login extends Component {
 					<h5>Welcome to simpList</h5>
 				</div>
 			
-				<Paper style={paperStyle} zDepth={3}>
-						<form>							
-							<TextField id="email" type="email" hintText="You Email Address" floatingLabelText="Enter Email Address:" style={textFieldStyle} underlineStyle={{display:'none'}}/>
-							{/*<input id="email" type="email"  placeholder="email" className="validate" />*/}
-							<br/>
-							<TextField id="password" type="password" hintText="Your Password" floatingLabelText="Enter Password" style={textFieldStyle} underlineStyle={{display:'none'}}/>
-							{/*<input id="password" type="password" placeholder="password" className="validate" />*/}
-							<br/><br/>
-							<RaisedButton label="Login" primary={true} style={buttonStyle} onClick={this.handleClick} onTouchTap={this.handleTouchTap}/>
-						</form>				
-				</Paper>			
-				
+				<Paper style={paperStyle1} zDepth={3}>
+				<div className="col-sm-6">
+					<form>							
+						<TextField id="email" type="email" hintText="You Email Address" floatingLabelText="Enter Email Address:" style={textFieldStyle} underlineStyle={{display:'none'}}/>
+						{/*<input id="email" type="email"  placeholder="email" className="validate" />*/}
+						<br/>
+						<TextField id="password" type="password" hintText="Your Password" floatingLabelText="Enter Password" style={textFieldStyle} underlineStyle={{display:'none'}}/>
+						{/*<input id="password" type="password" placeholder="password" className="validate" />*/}
+						<br/><br/>
+						<RaisedButton label="Login" primary={true} style={buttonStyle} onClick={this.handleClick} onTouchTap={this.handleTouchTap}/>
+						<RaisedButton label="New User" secondary={true} style={buttonStyle} onClick={this.handleClick} onTouchTap={this.handleTouchTap}/>
+					</form>				
+				</div>
+
+				<div className="col-sm-6">
+					<Paper style={paperStyle2} zDepth={1}>
+						<p id="onScreenTxt">If you are a new user please enter your info and press the New User button</p>
+					</Paper>
+				</div>
+				</Paper>	
+
 			</div>
 		);
 	}
