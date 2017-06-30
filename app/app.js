033ae7706493712c36c56a5a1a8b03ef81498d89
Stephen Pino <spino725@gmail.com>
2017-06-29 21:28:58 -0500

    fixing react router

--- app/app.js
+++ app/app.js
@@ -1,13 +1,8 @@
 // Include the Main React Dependencies
 import React from "react";
 import ReactDOM from "react-dom";
-import routes from "./config/routes";
+import { makeMainRoutes } from "./config/routes";
 
-// Including our Panel and Container components
-import Login from "./components/Login";
-import SavedPanel from "./components/SavedPanel";
-import SharedPanel from "./components/SharedPanel";
-import Profile from "./components/Profile";
-import CreateList from "./components/CreateList";
+const routes = makeMainRoutes();
 
 ReactDOM.render(routes, document.getElementById("app"));
