d94886ef29462c8d6619e50dad9e685d2e437daf
Stephen Pino <spino725@gmail.com>
2017-07-08 13:23:13 -0500

    added snackbar and clear form

--- app/components/CreateList.js
+++ app/components/CreateList.js
@@ -7,56 +7,71 @@ import Navbard from './Navbar.js';
 import RaisedButton from "material-ui/RaisedButton";
 import Paper from "material-ui/Paper";
 import TextField from "material-ui/TextField";
+import Snackbar from 'material-ui/Snackbar';
 
 
 
 //component used for creating a new list. Uses InputBar and ListItem
 class CreateList extends Component {
 
-    constructor(props){
-        super(props);
-        this.state = {
-            todoList:[],
-            listName: '',
-            addNewTask:(taskText)=>{
-              if (taskText == ''){ return; } 
-              let newItem = {task: taskText, completed: false};
-              let currentList = [...this.state.todoList];
-              currentList.push(newItem);
-              this.setState({
-                todoList: currentList
-              });
-            }
-        };
-        this.handleChange = this.handleChange.bind(this);  
-        this.handleSubmit = this.handleSubmit.bind(this); 
-        this.onRequestDelete = this.onRequestDelete.bind(this);
-        this.handleTouchTap = this.handleTouchTap.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      todoList: [],
+      listItemInput: '',
+      newItem: '',
+      addNewTask: (taskText) => {
+        if (taskText == '') { return; }
+        let newItem = { task: taskText, completed: false };
+        let currentList = [...this.state.todoList];
+        currentList.push(newItem);
+        this.setState({
+          todoList: currentList,
+          listItemInput: '',
+          newItem: { task: taskText, completed: false } 
+        });
+      }
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleRequestClose = this.handleRequestClose.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.onRequestDelete = this.onRequestDelete.bind(this);
+    this.handleTouchTap = this.handleTouchTap.bind(this);
+  };
 
-    handleTouchTap(){
-      console.log("mobile device touch");
-    }
-  
-    onRequestDelete(index) {
-      this.todoList = this.state.todoList;
-      console.log(this.todoList[0]);
-      const chipToDelete = index;
-      // const chipToDelete = this.todoList.map((chip, index) => chip.index).indexOf();
-      console.log("ChipToDelete " + chipToDelete);
-      this.todoList.splice(chipToDelete,1);
-      this.setState({todoList: this.todoList});      
-    };
+  handleTouchTap() {
+    this.setState({
+      open: true,
+    });
+    console.log("mobile device touch");
+  }
 
-    //handle text input in list name text box
-    handleChange(key) {
-      return function(e){
-        let state = {};
-        state[key] = e.target.value;
-        this.setState(state);
-        // console.log("55: " + this.state.listName);
-      }.bind(this);
-    };
+  handleRequestClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
+
+  onRequestDelete(index) {
+    this.todoList = this.state.todoList;
+    console.log(this.todoList[0]);
+    const chipToDelete = index;
+    // const chipToDelete = this.todoList.map((chip, index) => chip.index).indexOf();
+    console.log("ChipToDelete " + chipToDelete);
+    this.todoList.splice(chipToDelete, 1);
+    this.setState({ todoList: this.todoList });
+  };
+
+  //handle text input in list name text box
+  handleChange(key) {
+    return function (e) {
+      let state = {};
+      state[key] = e.target.value;
+      this.setState(state);
+      // console.log("55: " + this.state.listName);
+    }.bind(this);
+  };
 
   //handle form submit to grab state values
   handleSubmit(event) {
@@ -76,21 +91,29 @@ class CreateList extends Component {
 
     helpers.postList(finalList).then(function (data) {
       console.log(data);
+      this.clearForm();
     }.bind(this));
   }
 
-    render() {
-      const buttonStyle = {
-          margin: 20,
-      };
-      const textFieldStyle = {
-			    margin: 20,
-      };
+  clearForm() {
+      this.setState({
+        listName: "",
+        todoList: []
+      })
+    }
 
-        return (
-          <div>
-          <Navbard />
-          <div className="container">
+  render() {
+    const buttonStyle = {
+      margin: 20,
+    };
+    const textFieldStyle = {
+      margin: 20,
+    };
+
+    return (
+      <div>
+        <Navbard />
+        <div className="container">
           <div className="panel panel-default">
             <div className="panel-heading">
               <h5>Create a new list</h5>
@@ -98,21 +121,27 @@ class CreateList extends Component {
             <div className="panel-body">
               <InputBar addTask={this.state.addNewTask} />
               <div>
-                {this.state.todoList.map((item,index)=>{
+                {this.state.todoList.map((item, index) => {
                   console.log(item);
                   console.log("89 " + index);
                   return <ListItem task={item.task} key={index} value={index} taskIndex={index} onRequestDelete={this.onRequestDelete} handleTouchTap={this.handleTouchTap} />
                 })}
               </div>
               <form id="list-name-form" onSubmit={this.handleSubmit}>
-                <TextField id="save-list-name-txt" type="text" value={this.state.listName} onChange={this.handleChange('listName')} hintText="List Name" floatingLabelText="Enter a List Name" style={textFieldStyle} underlineStyle={{display:'none'}} />
-                <RaisedButton id="save-list-btn" type="submit" primary={true} label="Save List" />
+                <TextField id="save-list-name-txt" type="text" value={this.state.listName} onChange={this.handleChange('listName')} hintText="List Name" floatingLabelText="Enter a List Name" style={textFieldStyle} underlineStyle={{ display: 'none' }} />
+                <RaisedButton id="save-list-btn" type="submit" primary={true} onTouchTap={this.handleTouchTap} label="Save List" />
+                <Snackbar
+                  open={this.state.open}
+                  message="List successfully created."
+                  autoHideDuration={4000}
+                  onRequestClose={this.handleRequestClose}
+                />
               </form>
             </div>
           </div>
-          </div>
-          </div>
-        );
+        </div>
+      </div>
+    );
   }
 };
 
