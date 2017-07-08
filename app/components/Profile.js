a4e7e142827c348762fc255eadbf7b890d53d60c
Stephen Pino <spino725@gmail.com>
2017-07-07 21:39:46 -0500

    re adding nav bar and avatar in profile

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Route, Link } from "react-router-dom";
 import RaisedButton from "material-ui/RaisedButton";
+import Avatar from 'material-ui/Avatar';
 
 //initial profile page with buttons to navigate to create/saved/shared lists
 class Profile extends Component {
@@ -29,9 +30,11 @@ class Profile extends Component {
 		return (
 			<div className="panel panel-default">
 				<div className="panel-heading">
-					<h5>Welcome {profile.name}</h5>
+					<h5>Welcome {profile.name}
+						<Avatar src={profile.picture} />
+					</h5>
 				</div>
-				<pre>{JSON.stringify(profile, null, 2)}</pre>
+				{/*<pre>{JSON.stringify(profile, null, 2)}</pre>*/}
 
 				<div className="panel-body">
 					<div className="row">
