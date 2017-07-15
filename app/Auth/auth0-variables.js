99abf626e1bebff08efe1f20b21b7d827bcb5cec
Stephen Pino <spino725@gmail.com>
2017-07-14 20:03:59 -0500

    changing callback url

--- app/Auth/auth0-variables.js
+++ app/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'stephenpino.auth0.com',
   clientId: '-Q5qAGWdUPcFyKY_L5s67tLARFSZBx4K',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'https://frozen-basin-48164.herokuapp.com/callback'
 }
