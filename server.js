daaf1d4fb4f477beb15217ee51195fca64e1c458
Stephen Pino <spino725@gmail.com>
2017-07-14 20:14:29 -0500

    fixing server.js for heroku deploy

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ const routes = require("./routes/routes");
 const app = module.exports = express();
 const PORT = process.env.PORT || 3000;
 
-if(process.env.NODE_ENV !== "test"){
-  app.use(logger("dev"));
+if (process.env.NODE_ENV !== "test") {
+    app.use(logger("dev"));
 };
 
 app.use(bodyParser.json());
@@ -31,23 +31,41 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 app.use(express.static('./public'));
 app.use("/", routes);
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname + '/public/index.html'));
+app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 //DB configuration
 // -------------------------------------------------
 
-if (process.env.MONGODB_URI) {
-  var dbConnection = mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
+/*if (process.env.MONGODB_URI) {
+  var dbConnection = mongoose.connect("mongodb://heroku_8mcv9bb8:e3dmsa26osdpml40n59cknvm3v@ds151232.mlab.com:51232/heroku_8mcv9bb8", {useMongoClient: true});
 } else {
   var dbConnection = mongoose.connect("mongodb://localhost/simplist", {useMongoClient: true});
 }
-
 dbConnection.then(function(db){
-    app.listen(PORT, function() {
-        console.log("Connected to mongod. App listening on PORT: " + PORT);
+    app.listen(PORT, function () {
+        console.log("Connected to mongoose. App listening on PORT: " + PORT);
     });
-}).catch(function(err) {
+}).catch(function(err){
     console.log("Error connecting to mongoose")
+})*/
+
+if(process.env.MONGODB_URI){
+    mongoose.connect("mongodb://heroku_7632z27j:sj6ie58ir0p8fr5ik3onuttpj8@ds139072.mlab.com:39072/heroku_7632z27j")
+}else{
+     mongoose.connect("mongodb://localhost/simplist");
+}
+var db = mongoose.connection;
+
+db.on("error", function (err) {
+    console.log("Mongoose err: " + err);
+});
+
+//The server will listen only if the mongoose database is connected
+db.once("open", function () {
+    console.log("Mongo is connected!");
+    app.listen(PORT, function () {
+        console.log("App listening on port: " + PORT);
+    });
 });
